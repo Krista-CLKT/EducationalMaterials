5d07fe91bb60eb2ce2e70708d500ebc75ac6ae53
Krista <38170503+Krista-CLKT@users.noreply.github.com>
2018-04-14 01:36:34 -0700

    FortuneCookie/

---
+++ src/FortuneCookieDriver.java
@@ -0,0 +1,17 @@
+/*
+    Project Goals:
+        - Review constructor and method syntax.
+        - Utilize arrays and index iteration.
+        - Use an actionListener.
+        - Use JFrame and other Swing library tools.
+
+    NOTE: cookie.png image was found on
+    https://fortunehoroscope.com/cookie
+ */
+public class FortuneCookieDriver {
+
+    public static void main(String[] args) {
+        //initialize FortuneCookie object.
+        new FortuneCookie();
+    }
+}
